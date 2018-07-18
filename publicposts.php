3f660dab8c19d9b7e7e1f2e734843d996aad1027
Ajinkya <dube.ajinkya452@gmail.com>
2018-07-19 00:58:58 +0530

    changes

--- publicposts.php
+++ publicposts.php
@@ -1,22 +1,21 @@
 <?php
   include "header.php";
-    $sql = "SELECT * FROM posts";
+    $sql = "SELECT * FROM posts as p INNER JOIN users as u ON p.uid = u.id";
     $result = $conn->query($sql);
     if ($result->num_rows == 0) {
       //header("Location:step1.php");
     }else{
-      echo '<div class="col-md-8">';
       while($row = $result->fetch_assoc()) {
-          if($row['change_maker'] == 1){
+          if($row['p.change_maker'] == 1){
             $context = "I am the change!";
           }
           else {
             $context = "I saw the change!";
           }
-          $title = $row['title'];
-          $description = $row['description'];
+          $title = $row['p.title'];
+          $description = $row['p.description'];
       ?>
-          
+          <div class="col-md-8">
             <div class="card card-user">
               <div class="card-header">
                 <h5 class="card-title"><?php echo $title;?></h5>
@@ -24,12 +23,23 @@
               <div class="card-body">
                 <?php echo $description;?>
                 
+              </div>
             </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card card-user">
+              <div class="card-header">
+                <h5 class="card-title"><?php echo $context;?></h5>
+              </div>
+              <div class="card-body">
+                <?php echo $description;?>
+                
+              </div>
             </div>
+          </div>
         
 <?php
       }
-      echo '</div>';
     }
   include "footer.php";
 ?>
