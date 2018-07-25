bea4f7a7af42c74badb2c8b5bd27b3f23056ff98
Ajinkya <dube.ajinkya452@gmail.com>
2018-07-26 00:17:45 +0530

    changes

--- changemakers.php
+++ changemakers.php
@@ -13,14 +13,13 @@
     }else{
 
       ?>
-<div class="col-md-6">
   <?php
       while($row = $result->fetch_assoc()) {
           $vendor_id = $row['id'];
           $sql1 = "SELECT p.title,p.change_maker,u.name FROM posts as p INNER JOIN users as u ON p.uid = u.id AND p.vendor_id = ".$vendor_id;
           $result1 = $conn->query($sql1);
           while($row1 = $result1->fetch_assoc()) {
-            if($row['change_maker']%2 == 1){
+            if($row['change_maker']%2 == 0){
               $css = 'gradiamchange';
             }
             else {
@@ -28,7 +27,8 @@
             }
           
       ?>
-        <div class="card <?php echo $css;?>">
+        <div class="col-md-6">
+          <div class="card <?php echo $css;?>">
               <div class="card-header">
                 <h4 class="card-title"> <?php echo $row['name'];?></h4>
               </div>
@@ -38,14 +38,13 @@
               <div class="card-footer">
                 Posted By: <?php echo $row1['name'];?>
               </div>
-            </div>
+          </div>
+        </div>
 
       <?php 
         }
       }
       ?>
-
-    </div>
     <a class="btn btn-round btn-primary" href="changemakers.php?page=<?php echo $nextpage;?>" >Next</button>
             
 <?php
